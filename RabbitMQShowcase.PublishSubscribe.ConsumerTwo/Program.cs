2c9f189a69f161bd612dc56448c527b582d38291
Дмитрий Ляпкин <d.liapkin@gmail.com>
2025-09-27 08:48:15 +0300

    [Add]: added publish/subscribe example

---
+++ RabbitMQShowcase.PublishSubscribe.ConsumerTwo/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQShowcase.PublishSubscribe.ConsumerTwo;
+
+internal class ConsumerTwo
+{
+    static async Task Main(string[] args)
+    {
+        var exchangeName = "logs";
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.ExchangeDeclareAsync(
+                exchange: exchangeName,
+                type: ExchangeType.Fanout);
+
+        var queue = await channel.QueueDeclareAsync();
+        await channel.QueueBindAsync(queue: queue.QueueName, exchange: exchangeName, routingKey: string.Empty);
+
+        Console.WriteLine(" [*] Consumer Two [*]");
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            Thread.Sleep(500);
+
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received: {message}");
+
+            return Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(
+            queue: queue.QueueName,
+            autoAck: true,
+            consumer: consumer);
+
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQShowcase.PublishSubscribe.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQShowcase.PublishSubscribe.Publisher;
+
+internal class Publisher
+{
+    static async Task Main(string[] args)
+    {
+        var counter = 1;
+        do
+        {
+            var delay = new Random().Next(500, 1500);
+            Thread.Sleep(delay);
+
+            var exchangeName = "logs";
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.ExchangeDeclareAsync(
+                exchange: exchangeName,
+                type: ExchangeType.Fanout);
+
+            string message = $"Log [{counter++}]";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(
+                exchange: exchangeName,
+                routingKey: string.Empty,
+                body: body);
+
+            Console.WriteLine($" [x] Sent: {message}");
+
+            if (counter > 30)
+            {
+                break;
+            }
+        }
+        while (true);
+    }
+}
