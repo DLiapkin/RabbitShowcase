70349275c764eb57bdc365ab7d8523e08b15b9a8
Дмитрий Ляпкин <d.liapkin@gmail.com>
2025-09-27 11:13:56 +0300

    [Add]: added routing example

---
+++ RabbitMQShowcase.Routing.ConsumerOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQShowcase.Routing.ConsumerOne;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var exchangeName = "direct-logs";
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.ExchangeDeclareAsync(
+            exchange: exchangeName,
+            autoDelete: true,
+            type: ExchangeType.Direct);
+
+        var queue = await channel.QueueDeclareAsync();
+        await channel.QueueBindAsync(queue: queue.QueueName, exchange: exchangeName, routingKey: "Info");
+
+        Console.WriteLine(" [*] Consumer One [*]");
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            Thread.Sleep(500);
+
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received: {message}");
+
+            return Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(
+            queue: queue.QueueName,
+            autoAck: true,
+            consumer: consumer);
+
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQShowcase.Routing.Publisher/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQShowcase.Routing.Publisher;
+
+internal class Publisher
+{
+    static async Task Main(string[] args)
+    {
+        var counter = 1;
+        var message = string.Empty;
+        var dictionary = new Dictionary<int, string>()
+        {
+            { 1, "Info" },
+            { 2, "Warning" },
+            { 3, "Error" },
+        };
+
+        do
+        {
+            var delay = new Random().Next(500, 1500);
+            Thread.Sleep(delay);
+
+            var exchangeName = "direct-logs";
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.ExchangeDeclareAsync(
+                exchange: exchangeName,
+                autoDelete: true,
+                type: ExchangeType.Direct);
+
+            var severity = new Random().Next(1, 3);
+            message = $"[{dictionary[severity]}]: Log {counter++}";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(
+                exchange: exchangeName,
+                routingKey: dictionary[severity],
+                body: body);
+
+            Console.WriteLine($" [x] Sent: {message}");
+
+            if (counter > 30)
+            {
+                break;
+            }
+        }
+        while (true);
+    }
+}
